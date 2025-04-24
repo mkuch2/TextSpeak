db0f59095deb08549e220a911195cff7ed49244b
nessa :P <166348458+GTCcraft@users.noreply.github.com>
2025-04-23 19:30:38 -0700

    Update CreatePost.jsx

--- TextSpeak/src/components/CreatePost.jsx
+++ TextSpeak/src/components/CreatePost.jsx
@@ -1,9 +1,45 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function CreatePost() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
   return (
     <div>
-      <h1>This is the post place</h1>
+      <h1>Create post</h1>
+      <form onSubmit={handleSubmit}>
+      <label>Title:
+      <input 
+        type="text" 
+        name="postTitle" 
+        value={inputs.postTitle || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <label>Enter your age:
+        <input 
+          type="number" 
+          name="age" 
+          value={inputs.age || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <input type="submit" />
+    </form>
+      
+      <button>
+        <Link to="/"> return </Link>
+      </button>
     </div>
   );
 }
